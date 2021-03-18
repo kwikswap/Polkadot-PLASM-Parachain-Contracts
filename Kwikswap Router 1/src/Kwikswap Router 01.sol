189bee3e2c5a9c213653004b390e5f46f2489b77
KwikSwap <73940078+kwikswap@users.noreply.github.com>
2021-03-18 14:06:11 +0500

    Update Kwikswap Router 01.sol

--- Kwikswap Router 1/src/Kwikswap Router 01.sol
+++ Kwikswap Router 1/src/Kwikswap Router 01.sol
@@ -55,7 +55,7 @@ library TransferHelper {
     }
 
     function safeTransfer(address token, address to, uint value) internal {
-        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
     }
@@ -67,6 +67,7 @@ library TransferHelper {
     }
 
     function safeTransferETH(address to, uint value) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
         (bool success,) = to.call{value:value}(new bytes(0));
         require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
     }
