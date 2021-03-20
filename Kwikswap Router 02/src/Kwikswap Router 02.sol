e745047cb60bd4b92acccc8031e4646e35f032d2
KwikSwap <73940078+kwikswap@users.noreply.github.com>
2021-03-21 00:10:58 +0500

    Update Kwikswap Router 02.sol

--- Kwikswap Router 02/src/Kwikswap Router 02.sol
+++ Kwikswap Router 02/src/Kwikswap Router 02.sol
@@ -41,7 +41,6 @@ library SafeMath {
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     function safeApprove(address token, address to, uint value) internal {
-        // bytes4(keccak256(bytes('approve(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: APPROVE_FAILED');
     }
